bf2d7574700f05d99a26b4f388db40eaf720832d
Analee Guerra <aguerra1508@gmail.com>
2020-12-12 21:39:07 -0600

    changes to portfolio cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,22 +2,28 @@ import React from "react";
 
 function Card(props) {
   return (
-    <div className="card">
-      <div className="content">
-        <ul >
-					<img src={props.image} className="imgport center" alt={props.title}></img>
-          <li>
-            <strong>Title:</strong> {props.title}
-          </li>
-          <li>
-            <a href={props.gitHubUrl} target="_blank">GitHub Repo</a>
-          </li>
-          <li>
-            <a href={props.deployedUrl} target="_blank">Deployed App</a>
-          </li>
-        </ul>
+		<div class="card mb-3" >
+  <div class="row no-gutters">
+    <div class="col-md-8">
+      <img src={props.image} className="card-img" alt={props.title}></img>
+    </div>
+    <div class="col-md-4">
+      <div class="card-body">
+				<ul>
+					<li>
+					<strong>Title: </strong>{props.title}
+					</li>
+					<li>
+					<a href={props.gitHubUrl} target="_blank">GitHub Repo</a>
+					</li>
+					<li>
+					<a href={props.deployedUrl} target="_blank">Deployed App</a>
+					</li>
+				</ul>
       </div>
     </div>
+  </div>
+</div>
   );
 }
 
